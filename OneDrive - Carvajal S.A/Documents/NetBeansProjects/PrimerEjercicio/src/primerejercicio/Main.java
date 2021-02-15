6b8f8835dc4216b0c5e469597520cf3c5b1ce1bb
yazvivbe <yazvivbe@CSTCO5CG92813WS.carvajal.com.co>
2021-02-14 21:10:44 -0500

    subiendo proyecto

---
+++ OneDrive - Carvajal S.A/Documents/NetBeansProjects/PrimerEjercicio/src/primerejercicio/Main.java
@@ -0,0 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.lang.*;
+
+/**
+ *
+ * @author yazvivbe
+ */
+public class Main {
+    
+   public static void main(String[] args) {
+   EnmascararCuenta mascara =new EnmascararCuenta();
+   mascara.Convertir();
+    
+}
+}
