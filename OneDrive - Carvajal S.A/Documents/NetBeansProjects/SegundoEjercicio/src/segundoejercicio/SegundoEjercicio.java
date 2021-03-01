4b585fb84808408876b5baa5d053a988a67c461f
yazvivbe <yazvivbe@CSTCO5CG92813WS.carvajal.com.co>
2021-02-28 22:46:41 -0500

    Commit para tercer ejercicio

--- OneDrive - Carvajal S.A/Documents/NetBeansProjects/SegundoEjercicio/src/segundoejercicio/SegundoEjercicio.java
+++ OneDrive - Carvajal S.A/Documents/NetBeansProjects/SegundoEjercicio/src/segundoejercicio/SegundoEjercicio.java
@@ -1,5 +1,10 @@
 package segundoejercicio;
+import com.sun.org.apache.xerces.internal.xs.XSNotationDeclaration;
 import java.util.*;
+import static java.util.Collections.list;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import static javafx.scene.input.KeyCode.R;
 
 /**
  *
@@ -12,11 +17,13 @@ public class SegundoEjercicio {
      */
     public static void main(String[] args) {
         
-          Empleado emp1 = new Empleado ("Carlos","Torres", 12345, 250000);
-          Empleado emp2 = new Empleado ("Sofia","Mejia",  12346, 250000);
-          Empleado emp3 = new Empleado ("Lorena","Cajiado", 12347, 250000);
+          Empleado emp1 = new Empleado ("Carlos","Torres", 12345, 50000);
+          Empleado emp2 = new Empleado ("Sofia","Mejia",  12346,  80000);
+          Empleado emp3 = new Empleado ("Lorena","Cajiado", 12347, 90000);
+          Empleado emp4 = new Empleado ("Lorena","Cajiado", 12347, 270000);
                   // TODO code application logic here
         
+                  
         Set<Empleado> coleccionEmpleado=new HashSet<Empleado>();
         
         coleccionEmpleado.add(emp1);
@@ -28,6 +35,32 @@ public class SegundoEjercicio {
         {
             System.out.println(empleado.getNombre() +" "+ empleado.getNombre()  + " " +empleado.getId());
         }
+        
+        //Convierte  la lista en streams
+        Stream<Empleado> nombreCompleto;
+        nombreCompleto = coleccionEmpleado.stream().filter(coleccionEmpleado-> coleccionEmpleado.getNombre());
+         
+         
+        
+        //Empleado con Mayor Salario
+        System.out.println("El mayor salario");
+        
+         //Empleado con Menor  Salario
+        System.out.println("El mayor salario");
+        
+         //Ordenar por nombre
+        System.out.println("El mayor salario");
+        
+         //Suma de Salario mayor a 70000
+        System.out.println("El mayor salario");
+        
+         //Empleado primeros 5 salarios con el mayor salario
+        System.out.println("El mayor salario");
+        
+         //Numero de empleados cuyo primer aapellido empice por A
+        System.out.println("El mayor salario");
+        
+        
         
         
     }
