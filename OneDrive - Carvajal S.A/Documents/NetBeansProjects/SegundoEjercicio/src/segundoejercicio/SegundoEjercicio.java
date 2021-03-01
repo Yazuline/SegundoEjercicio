a6100bad8f0c6a3aedff00e7baf4a1ca3a776514
yazvivbe <yazvivbe@CSTCO5CG92813WS.carvajal.com.co>
2021-02-28 23:30:24 -0500

    Commit para tercer ejercicio

--- OneDrive - Carvajal S.A/Documents/NetBeansProjects/SegundoEjercicio/src/segundoejercicio/SegundoEjercicio.java
+++ OneDrive - Carvajal S.A/Documents/NetBeansProjects/SegundoEjercicio/src/segundoejercicio/SegundoEjercicio.java
@@ -1,5 +1,4 @@
 package segundoejercicio;
-import com.sun.org.apache.xerces.internal.xs.XSNotationDeclaration;
 import java.util.*;
 import static java.util.Collections.list;
 import java.util.function.Function;
@@ -17,53 +16,47 @@ public class SegundoEjercicio {
      */
     public static void main(String[] args) {
         
-          Empleado emp1 = new Empleado ("Carlos","Torres", 12345, 50000);
+          Empleado emp1 = new Empleado ("Carlos","Arias", 12345, 50000);
           Empleado emp2 = new Empleado ("Sofia","Mejia",  12346,  80000);
-          Empleado emp3 = new Empleado ("Lorena","Cajiado", 12347, 90000);
+          Empleado emp3 = new Empleado ("Lucia","Cajiado", 12347, 90000);
           Empleado emp4 = new Empleado ("Lorena","Cajiado", 12347, 270000);
                   // TODO code application logic here
         
                   
-        Set<Empleado> coleccionEmpleado=new HashSet<Empleado>();
+        Set<Empleado> coleccionEmpleado=new HashSet<>();
         
         coleccionEmpleado.add(emp1);
         coleccionEmpleado.add(emp2);
         coleccionEmpleado.add(emp3);
         coleccionEmpleado.add(emp4);
         
-        for(Empleado empleado: coleccionEmpleado )
-        {
+        coleccionEmpleado.stream().map((empleado) -> {
             System.out.println(empleado.getNombre() +" "+ empleado.getNombre()  + " " +empleado.getId());
-        }
-        
-        //Convierte  la lista en streams
-        Stream<Empleado> nombreCompleto;
-        nombreCompleto = coleccionEmpleado.stream().filter(coleccionEmpleado-> coleccionEmpleado.getNombre());
-         
-         
-        
-        //Empleado con Mayor Salario
-        System.out.println("El mayor salario");
-        
-         //Empleado con Menor  Salario
-        System.out.println("El mayor salario");
-        
-         //Ordenar por nombre
-        System.out.println("El mayor salario");
-        
-         //Suma de Salario mayor a 70000
-        System.out.println("El mayor salario");
-        
-         //Empleado primeros 5 salarios con el mayor salario
-        System.out.println("El mayor salario");
-        
-         //Numero de empleados cuyo primer aapellido empice por A
-        System.out.println("El mayor salario");
-        
-        
-        
-        
-    }
+            return empleado;
+        }).map((empleado) -> {
+            if (empleado.getSalario()> 10000)
+            {
+                
+                System.out.println(empleado.getNombre() + " Gana " +empleado.getSalario());
+            }
+            return empleado;
+        }).map((empleado) -> {
+            if (empleado.getSalario() < 70000)
+                
+            {
+                System.out.println(empleado.getNombre() + " Gana menos de (70000)  tan solo  gana =" +empleado.getSalario());
+            }
+            return empleado;
+        }).forEachOrdered((empleado) -> {
+            list<String>nombreCompleto = (list<String>) coleccionEmpleado.stream()
+                    .filter(empleados->empleado.getSalario()>70000);
+            
+            System.out.println(empleado.getNombre() + " Gana metodo  " + nombreCompleto);
+        });
+   
 }
 
-    
+        
+    }
+
+ 
